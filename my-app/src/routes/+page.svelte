6122a3cc6a31b39f1db74b28086492f5355d310a
Paul Thomas <paulthomas2107@gmail.com>
2023-02-19 16:17:57 +0000

    Fixes

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,23 +1,39 @@
 <script lang="ts">
 	import ProductCard from '$lib/productCard.svelte';
-
+	import { get } from 'svelte/store';
+	import { cartItems } from '../cart';
 	const products: Product[] = [
 		{
-			id: '1',
+			id: 'price_1LufAcDAv1ILAQyV9f0B3xy3',
 			name: 'Coffee',
 			price: 5
 		},
 		{
-			id: '2',
+			id: 'price_1LufCYDAv1ILAQyVFMoUHKKi',
 			name: 'Sunglasses',
-			price: 25
+			price: 10
 		},
 		{
-			id: '3',
-			name: 'Camera Lens',
-			price: 512
+			id: 'price_1LufDcDAv1ILAQyVi9LbegO3',
+			name: 'Camera',
+			price: 15
 		}
 	];
+	async function checkout() {
+		await fetch('api/stripeCheckout', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ items: get(cartItems) })
+		})
+			.then((data) => {
+				return data.json();
+			})
+			.then((data) => {
+				window.location.replace(data.url);
+			});
+	}
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
@@ -25,13 +41,13 @@
 		<div class="col-span-3">
 			<h1>SvelteKit 1.0 Store</h1>
 		</div>
-
 		{#each products as product}
-			<ProductCard product={product}/>
+			<ProductCard {product} />
 		{/each}
-
 		<div class="col-span-3">
-			<button class="btn variant-filled-primary">Checkout with Stripe API</button>
+			<button class="btn variant-filled-primary" on:click={() => checkout()}
+				>Checkout with Stripe API</button
+			>
 		</div>
 	</div>
 </div>
