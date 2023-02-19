33fb7970ead4f135bb251ee3b13938e236df7309
Paul Thomas <paulthomas2107@gmail.com>
2023-02-19 13:53:15 +0000

    More....

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,10 +1,13 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import ProductCard from '$lib/productCard.svelte';
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="grid grid-cols-3 gap-4">
 		<div class="col-span-3">
 			<h1>SvelteKit 1.0 Store</h1>
 		</div>
+		<ProductCard />
 		<div class="col-span-3">
 			<button class="btn variant-filled-primary">Checkout with Stripe API</button>
 		</div>
